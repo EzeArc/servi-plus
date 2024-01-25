f00d4a9f6ce3ffea005e020cc0d50a4efbf96026
Ezequiel <ezequielarce_1996@hotmail.com>
2024-01-25 11:11:48 -0300

    Creation of the backend folder with Spring Starter web

---
+++ sp_back/src/main/java/serviplus/sp_back/SpBackApplication.java
@@ -0,0 +1,13 @@
+package serviplus.sp_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpBackApplication.class, args);
+	}
+
+}
